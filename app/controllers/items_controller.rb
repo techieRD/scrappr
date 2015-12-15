1a443cc88f767db232aa44e26874119bae27ed57
hamholla <hamhollett@gmail.com>
2015-12-15 09:28:42 -0600

    added item ownership with taytay

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -70,6 +70,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params #permit but no require?
-      params.require(:item).permit(:name, :description, :condition, :price, :image, :category_id, :street_address)
+      params.require(:item).permit(:name, :description, :condition, :price, :image, :category_id, :street_address, :user_id)
     end
 end
